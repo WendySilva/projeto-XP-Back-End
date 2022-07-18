529f35c764ed4f3281f092097415bfcb8c468db4
Wendy Silva <silva.wendy.souza@gmail.com>
2022-07-17 21:41:23 -0300

    feat: router de investimentos

---
+++ src/Routers/investimentos.router.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const investimentosRouter = express.Router();
+
+investimentosRouter.post('/comprar', );
+
+export default investimentosRouter;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const { default: investimentosRouter } = require('./Routers/investimentos.router');
 
 const app = express();
 
 app.use(express.json());
 
 const port = process.env.PORT;
+
+app.use('/investimentos', investimentosRouter);
 
 app.listen(port, () => console.log('listen port', port));
