1d56d7f32214dd8f5478b20161c90599e108550f
Wendy Silva <silva.wendy.souza@gmail.com>
2022-07-24 17:22:32 -0300

    permitindo que só o proprio usuario compre invetsimentos para na sua ocnta

--- src/controllers/investimentos.controller.js
+++ src/controllers/investimentos.controller.js
@@ -1,11 +1,17 @@
 const serviceInvestimentos = require('../services/investimentos.service');
+const { tokenUsuario } = require('../uteis/JWTtoken');
 
 const novaCompra = async (req, res, next) => {
-  const resposta = await serviceInvestimentos.novaCompra(req.body);
+  const { codCliente } = tokenUsuario(req.headers.authorization);
+
+  if (codCliente === req.body.codCliente) {
+    const resposta = await serviceInvestimentos.novaCompra(req.body);
   if(resposta !== undefined) res.status(201).json(
     { message: `A compra foi efetuada, seu saldo atual é de ${resposta.toFixed(2)}`}
   );
   return next({ status: 400, message: `Seu saldo é de não foi suficiente para a compra`});
+  }
+  return next({ status: 401, message: 'Essa conta não pertence esse a esse usuário' });
 };
 
 module.exports = {
